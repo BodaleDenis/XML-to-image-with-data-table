eb6be93f980c02370a9a67de450d078667c89c7e
Bodale Denis <bodaledenis.work@gmail.com>
2021-10-01 12:35:47 +0300

    test

--- src/XmlParser.py
+++ src/XmlParser.py
@@ -1,21 +1,22 @@
 import xml.etree.ElementTree as ET
 import sys
 from os import path
+
 sys.path.append("./")
 
-class XmlParser:
 
+class XmlParser:
     def __init__(self, doc_name: str):
         self.parser = ET.parse(doc_name).getroot()
         self.root = ET.parse(doc_name)
         self.doc_name = doc_name
-    
+
     def get_xml_root_name(self):
         return self.parser.tag
-    
+
     def get_xml_root_value(self):
         return self.parser.attrib
-    
+
     def get_node_tag_and_value(self, index: int):
         children_tag = self.parser[index].tag
         children_value = self.parser[index].attrib
@@ -30,11 +31,11 @@ class XmlParser:
             elements.append(element_value_tag)
         return elements
 
-#Driver code
+
+# Driver code
 xmlparser = XmlParser("test/dummy.xml")
-print(len(xmlparser.parser))
+# print(len(xmlparser.parser))
 print(xmlparser.get_xml_root_name())
 print(xmlparser.get_xml_root_value())
 print(xmlparser.get_node_tag_and_value(0))
 print(xmlparser.get_values_of_child_node(0))
-
